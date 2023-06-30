11e4f2a81b5b8b5ffc9be4a647b25db01749d06e
Danny Vinicio Cobos Vega <danny.cobos@unl.edu.ec>
2023-06-29 22:10:00 -0500

    Correcciones

--- PIS_PROYECTO/src/controlador/DAO/InterfazDAO.java
+++ PIS_PROYECTO/src/controlador/DAO/InterfazDAO.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controller.DAO;
+package controlador.DAO;
 
 import controlador.ed.listas.ListaEnlazada;
 import controlador.ed.listas.exception.ListaNullException;
